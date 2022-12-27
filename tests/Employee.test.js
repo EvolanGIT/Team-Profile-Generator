d446a0eb772265ce8bfeb9a8b07724e5aacf22fd
Evolan <yon_baermund@hotmail.com>
2022-12-26 21:51:42 -0500

    employee test finished and passing, next to work on engineer.

---
+++ tests/Employee.test.js
@@ -0,0 +1,30 @@
+const Employee = require('../lib/employee');
+
+describe("Retrieve", () => {
+    it("should retrieve employee's name", () => {
+        const empName = 'Yon';
+        const testEmp = new Employee('Yon', 7, 'fake@mail.com', 'Employee');
+        expect(testEmp.retName()).toBe(empName);
+    });
+
+    it("should retrieve employee's role", () => {
+        const empRole = 'Employee';
+        const testEmp = new Employee('Yon', 7, 'fake@mail.com', 'Employee');
+        expect(testEmp.retRole()).toBe(empRole);
+    });
+
+    it("should retrieve employee's ID", () => {
+        const empID = 7;
+        const testEmp = new Employee('Yon', 7, 'fake@mail.com', 'Employee');
+        expect(testEmp.retID()).toBe(empID);
+    });
+
+    it("should retrieve employee's Email", () => {
+        const empMail = 'fake@mail.com';
+        const testEmp = new Employee('Yon', 7, 'fake@mail.com', 'Employee');
+        expect(testEmp.retMail()).toBe(empMail);
+    });
+
+    
+
+});
