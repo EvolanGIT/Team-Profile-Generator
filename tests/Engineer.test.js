0e6f8088991e584c70b3d4e8f3bca58ce7ecadf5
Evolan <yon_baermund@hotmail.com>
2022-12-26 23:22:12 -0500

    intern testing working, Manager test to continue.

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -2,32 +2,32 @@ const Engineer = require('../lib/engineer');
 
 describe("Retrieve", () => {
     it("should retrieve engineer's name", () => {
-        const empName = 'Yon';
-        const testEmp = new Engineer('Yon', 7, 'fake@mail.com', 'Employee', 'fakeGit');
+        const empName = 'Tony';
+        const testEmp = new Engineer('Tony', 5, 'fake@mail.com', 'Engineer', 'fakeGit');
         expect(testEmp.retName()).toBe(empName);
     });
 
     it("should retrieve engineer's role", () => {
-        const empRole = 'Employee';
-        const testEmp = new Engineer('Yon', 7, 'fake@mail.com', 'Employee', 'fakeGit');
+        const empRole = 'Engineer';
+        const testEmp = new Engineer('Tony', 5, 'fake@mail.com', 'Engineer', 'fakeGit');
         expect(testEmp.retRole()).toBe(empRole);
     });
 
     it("should retrieve engineer's ID number", () => {
-        const empID = 7;
-        const testEmp = new Engineer('Yon', 7, 'fake@mail.com', 'Employee', 'fakeGit');
+        const empID = 5;
+        const testEmp = new Engineer('Tony', 5, 'fake@mail.com', 'Engineer', 'fakeGit');
         expect(testEmp.retID()).toBe(empID);
     });
 
     it("should retrieve engineer's Email", () => {
         const empMail = 'fake@mail.com';
-        const testEmp = new Engineer('Yon', 7, 'fake@mail.com', 'Employee', 'fakeGit');
+        const testEmp = new Engineer('Tony', 5, 'fake@mail.com', 'Engineer', 'fakeGit');
         expect(testEmp.retMail()).toBe(empMail);
     });
 
     it("should retrieve engineer's Github Alias", () => {
         const empGit = 'fakeGit';
-        const testEmp = new Engineer('Yon', 7, 'fake@mail.com', 'Employee', 'fakeGit');
+        const testEmp = new Engineer('Tony', 5, 'fake@mail.com', 'Engineer', 'fakeGit');
         expect(testEmp.retGit()).toBe(empGit);
     });
 
--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,34 +1,34 @@
-const Intern = require('./employee');
+const Intern = require('../lib/intern');
 
 describe("Retrieve", () => {
-    it("should retrieve employee's name", () => {
-        const empName = 'Yon';
-        const testEmp = new Employee('Yon', 7, 'fake@mail.com', 'Employee');
+    it("should retrieve intern's name", () => {
+        const empName = 'Harry';
+        const testEmp = new Intern('Harry', 9, 'fake@mail.com', 'Intern', 'Hogwarts');
         expect(testEmp.retName()).toBe(empName);
     });
 
-    it("should retrieve employee's role", () => {
-        const empRole = 'Employee';
-        const testEmp = new Employee('Yon', 7, 'fake@mail.com', 'Employee');
+    it("should retrieve interns's role", () => {
+        const empRole = 'Intern';
+        const testEmp = new Intern('Harry', 9, 'fake@mail.com', 'Intern', 'Hogwarts');
         expect(testEmp.retRole()).toBe(empRole);
     });
 
-    it("should retrieve employee's ID", () => {
-        const empID = 7;
-        const testEmp = new Employee('Yon', 7, 'fake@mail.com', 'Employee');
+    it("should retrieve intern's ID", () => {
+        const empID = 9;
+        const testEmp = new Intern('Harry', 9, 'fake@mail.com', 'Inter', 'Hogwarts');
         expect(testEmp.retID()).toBe(empID);
     });
 
-    it("should retrieve employee's Email", () => {
+    it("should retrieve intern's Email", () => {
         const empMail = 'fake@mail.com';
-        const testEmp = new Employee('Yon', 7, 'fake@mail.com', 'Employee');
+        const testEmp = new Intern('Harry', 9, 'fake@mail.com', 'Intern', 'Hogwarts');
         expect(testEmp.retMail()).toBe(empMail);
     });
 
-    it("should retrieve employee's school", () => {
-        const empMail = 'fake@mail.com';
-        const testEmp = new Employee('Yon', 7, 'fake@mail.com', 'Employee');
-        expect(testEmp.retMail()).toBe(empMail);
+    it("should retrieve intern's School", () => {
+        const empSchool = 'Hogwarts';
+        const testEmp = new Intern('Harry', 9, 'fake@mail.com', 'Intern', 'Hogwarts');
+        expect(testEmp.retSchool()).toBe(empSchool);
     });
 
 });
